be6ae295d92c86069bc56c75ab9a4a4625343235
Diggory Briercliffe <dig@amee.cc>
2010-05-24 15:00:52 +0100

    PL-1790 - Improved cache invalidation. PL-1790 - Efficiently handle new DataCategories in the path item cache. PL-1790 - Allow invalidation (and other) messages to be marked and handled as locally or remotely sourced.

--- src/main/java/com/amee/messaging/Message.java
+++ src/main/java/com/amee/messaging/Message.java
@@ -4,8 +4,11 @@ import org.springframework.context.ApplicationEvent;
 
 public abstract class Message extends ApplicationEvent {
 
+    private boolean local = false;
+
     public Message(Object source) {
         super(source);
+        setLocal(true);
     }
 
     public Message(Object source, String message) {
@@ -16,4 +19,12 @@ public abstract class Message extends ApplicationEvent {
     public abstract String getMessage();
 
     public abstract void setMessage(String message);
+
+    public boolean isLocal() {
+        return local;
+    }
+
+    public void setLocal(boolean local) {
+        this.local = local;
+    }
 }
